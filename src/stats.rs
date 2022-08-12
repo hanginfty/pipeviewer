cf8ea37217cfc25792ac6a33b1fbe1babbdaf7c3
hanginfty <223126765@qq.com>
2022-08-12 16:28:51 +0800

    refactor: timer  mod

--- src/stats.rs
+++ src/stats.rs
@@ -1,3 +1,5 @@
+mod timer;
+
 use crossbeam::channel::Receiver;
 use crossterm::{
     cursor, execute,
@@ -5,7 +7,8 @@ use crossterm::{
     terminal::{Clear, ClearType},
 };
 use std::io::{self, Result, Stderr, Write};
-use std::time::{Duration, Instant};
+use std::time::Instant;
+use timer::Timer;
 
 fn output_progress(stderr: &mut Stderr, bytes: usize, elapsed: String, rate: f64) {
     let bytes = style::style(format!("{} ", bytes)).with(Color::Red);
@@ -70,39 +73,5 @@ impl TimeOutput for u64 {
         let (hours, left) = (*self / 3600, *self % 3600);
         let (min, sec) = (left / 60, left % 60);
         format!("{}:{:02}:{02}", hours, min, sec)
-    }
-}
-
-struct Timer {
-    last_instant: Instant,
-    delta: Duration,
-    period: Duration,
-    countdown: Duration,
-    ready: bool,
-}
-
-impl Timer {
-    fn new() -> Self {
-        let now = Instant::now();
-
-        Self {
-            last_instant: now,
-            delta: Duration::default(),
-            period: Duration::from_millis(1000),
-            countdown: Duration::default(),
-            ready: true,
-        }
-    }
-
-    fn update(&mut self) {
-        let now = Instant::now();
-
-        self.delta = now - self.last_instant;
-        self.last_instant = now;
-
-        self.countdown = self.countdown.checked_sub(self.delta).unwrap_or_else(|| {
-            self.ready = true;
-            self.period
-        });
     }
 }
